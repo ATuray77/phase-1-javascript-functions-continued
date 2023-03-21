0d329584437046ae19486259f88fc82e11a4334d
Aruna Turay <tura_arun@yahoo.com>
2023-03-21 02:16:52 -0400

    grouping

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(play = "roller-skate") {
+  return `This Saturday, I want to ${play}!`;
+}
+
+const mondayWork = function(work = "go to the office") {
+  return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective (cool = '*'){
+  return function(name = "special"){
+    return `You are ${cool}${name}${cool}!`
+
+  }
+ 
+}
